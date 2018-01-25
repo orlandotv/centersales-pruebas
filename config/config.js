17c46d9876c0841b46b266048f72f23d29b9bbd2
orlandotv <orlando.trujillo@bleinconsulting.com>
2018-01-25 11:55:51 -0600

    https instead of http

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     clientId : '3MVG9Vik22TUgUphhb7LfjCjhOCKEpUKdCSVcUrLS7CLwFTxVfIDQcA0rNDUYIzGbbJUabkjaJPP0wySztCse',
     clientSecret : '3131942360529646838',
-    redirectUri : 'http://centersales-pruebas.herokuapp.com/oauth2/callback',
+    redirectUri : 'https://centersales-pruebas.herokuapp.com/oauth2/callback',
     logInUrlDev: 'https://test.salesforce.com',
     logInUrlProd: 'https://login.salesforce.com',
     user: 'adiez@sorteostec.dev.sorteoback',
