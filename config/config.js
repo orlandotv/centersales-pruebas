95b1730d1147f51076256815d997ffdda133384d
orlandotv <orlando.trujillo@bleinconsulting.com>
2018-01-25 11:49:20 -0600

    changing redirectURI

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     clientId : '3MVG9Vik22TUgUphhb7LfjCjhOCKEpUKdCSVcUrLS7CLwFTxVfIDQcA0rNDUYIzGbbJUabkjaJPP0wySztCse',
     clientSecret : '3131942360529646838',
-    redirectUri : 'http://localhost:8080/oauth2/callback',
+    redirectUri : 'http://centersales-pruebas.herokuapp.com/oauth2/callback',
     logInUrlDev: 'https://test.salesforce.com',
     logInUrlProd: 'https://login.salesforce.com',
     user: 'adiez@sorteostec.dev.sorteoback',
